5796b6a5b2d6cd87989b9be6669b5037c430c6dc
indypottie <indy.pottie@telenet.be>
2026-07-17 18:40:40 +0200

    refactor

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -164,7 +164,7 @@ export default function Navbar() {
 
                     <LinkButton
 
-                        href="/#contact"
+                        to="/#contact"
 
                         variant="outlined"
 
